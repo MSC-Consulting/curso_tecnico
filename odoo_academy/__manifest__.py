fdd9abd826ecea3b6e982786b3bc74ad4cea60b8
MSC-Consulting <miguel.santoy@msc-consulting.group>
2021-11-04 04:34:55 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+{
+    
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage training""",
+    
+    'description': """
+        Academy module to manage training:
+        - Courses
+        - Sessions
+        - Attendees
+        """,
+    
+    'author': 'Odoo',
+    
+    'website': 'https://www.oddo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+        
+    ],
+
+}
