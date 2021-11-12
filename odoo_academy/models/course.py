dc78cc69b842a8a8b1eaa1974c6d90d78f603813
John Doe <johndoe@example.com>
2021-11-12 18:44:46 +0000

    [ADD] Basic Module

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -1,20 +1,10 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-from odoo import models,fields,api
+from odoo import models, fields, api
 
 class Course(models.Model):
+     _name = 'academy.course'
+    _description = 'Course Info'
     
-    _name = 'academy.course'
-    _description='Course Info'
     
-    name = fields.Char(string='Title',required=True)
-    description = fields.Text(string='Description')
-    
-    level = fields.Selection(string='Level',
-                            selection=[('beginner', 'Beginner'),
-                                       ('intermediate','Intermediate'),
-                                        ('advanced', 'Advanced')],
-                             
-                                        copy=False)
-    
-    active = fields.Boolean(string='Active', default=True)
+   
